5a0e97ea7d15a55dc5b7bd069d1fa50ba18abb80
DEYBER 1 <deyber147852@gmail.com>
2026-06-07 06:36:27 -0500

    fix: configurar CORS para Firebase Hosting

--- app/main.py
+++ app/main.py
@@ -25,7 +25,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://polla-mundialista-393f8.web.app",
+        "https://polla-mundialista-393f8.firebaseapp.com",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
