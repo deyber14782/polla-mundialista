21a9e125877ac02bbfbbed198f6c358298cebeb4
DEYBER 1 <deyber147852@gmail.com>
2026-06-08 21:10:08 -0500

    feat: midiendo tiempos

--- app/core/auth.py
+++ app/core/auth.py
@@ -2,17 +2,27 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from firebase_admin import auth
 from app.core.firebase import db
+import time
 
 security = HTTPBearer()
 
 async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    start = time.time()
     token = credentials.credentials
     try:
+        t1 = time.time()
         decoded_token = auth.verify_id_token(token, clock_skew_seconds=10)
+        print("verify_id_token:", round(time.time() - t1, 2))
+        uid = decoded_token["uid"]
+
         uid = decoded_token["uid"]
 
         user_ref = db.collection("users").document(uid)
+        t2 = time.time()
         user_doc = user_ref.get()
+        print("user_doc:", round(time.time() - t2, 2))
+
+        print("TOTAL AUTH:", round(time.time() - start, 2))
 
         if not user_doc.exists:
             raise HTTPException(
